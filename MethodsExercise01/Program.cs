bc68ce5d48d3cf08c50678f79b28b4e68b0c3d3a
lostinthestack <lostinthestack@protonmail.com>
2021-09-29 18:27:17 -0500

    Initial commit

---
+++ MethodsExercise01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MethodsExercise01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, animal, animalName, cityName, cityDescriptor;
+
+            Console.Write("What is your name:    ");
+            name = Console.ReadLine();
+            Console.Write("\nWhat kind of pet do you have:    ");
+            animal = Console.ReadLine();
+            Console.Write("\nWhat is your pet's name:    ");
+            animalName = Console.ReadLine();
+            Console.Write("\nWhat city do you live in:    ");
+            cityName = Console.ReadLine();
+            Console.Write("\nDescribe your city in one word:    ");
+            cityDescriptor = Console.ReadLine();
+
+            Console.WriteLine();
+
+            void Story()
+            {
+                Console.WriteLine($"{name} lives in {cityName} with his {animal} {animalName}.\n{cityName} is a {cityDescriptor} place so {name} and {animalName} \nspend their days dreaming of Colorado.");
+            }
+
+            Story();
+
+            // Example output:
+            // Blake lives in Birmingham with his dog Benny.
+            // Birmingham is a dreadful place so Blake and Benny
+            // spend their days dreaming of Colorado.
+        }
+    }
+}
